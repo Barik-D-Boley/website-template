0780b51f2720ec8a818d9b25b69d9f9ec77f242a
Barik-Boley <71157911+xBarikadex@users.noreply.github.com>
2022-03-21 12:37:58 -0700

    Finished the footer and resume page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express"),
     // passportLocalMongoose = require("passport-local-mongoose"),
     // User = require("./models/user");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
